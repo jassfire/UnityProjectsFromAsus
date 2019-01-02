45fe63ef5f841c59391af5e02dbcd7b8ea8105e2
jassfire <alikeshi@sina.com>
2019-01-02 16:15:39 +0800

    3Dtest

---
+++ hlstest/Assets/Scripts/Move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        //Debug.Log(h);
+        transform.Translate(new Vector3(h, v, 0) * Time.deltaTime * speed);
+    }
+}
